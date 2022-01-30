03f8c05d72e9679063b3e09a759dcf2f8dc35f2f
M2r1n <97593420+M2r1n@users.noreply.github.com>
2022-01-30 09:34:04 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  i <- NULL
+  set <- function(x) {
+    m <<- x
+    i <<- NULL
+  }
+  get <- function() m
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
@@ -12,4 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   ma <- x$get()
+  if(!is.numeric(ma)){
+    message("Please use a numeric matrix")
+  } else {
+    message("I get ur matrix")
+    ma1<-ma
+    ma <- solve(ma1,...)
+    x$setinverse(ma)
+    ma
+  }
 }
